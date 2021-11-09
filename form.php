d22e5371ae40c94b9f93d134adeb96110fc8d1e3
macacorosa <93205054+macacorosa@users.noreply.github.com>
2021-11-09 12:08:13 -0300

    commit
    ndsaf

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head> <meta charset="utf-8">
+	<meta name="viemport" content="width=device-width, initial-scale=1">
+	<title> Gravar dados em arquivos</title>
+</head>
+<body>
+<form action="valida.php" method="post">
+	<p>Nome:<input type="text" name="nome"></p>
+	<p>Idade:<input type="number" name="idade"></p>
+	<button type="submit"> Gravar no arquivo </button>
+</form>
+</body>
+</html>
